e9fd327761acc0581931c27d055161c76cb80311
Ac <stephane.achari@u-psud.fr>
2019-04-16 17:38:37 +0200

    models

--- printingForm/models.py
+++ printingForm/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  nom = models.CharField(max_length=50,null=False)
+  prenom = models.CharField(max_length=50,null=False)
+  email = models.CharField(max_length=100, null=False)
+  numero = models.CharField(max_length=50, null=False)
+
+class Building(models.Model):
+  building = models.CharField(max_length=50, null=False)
+
+class Office(models.Model):
+  officeName = models.CharField(max_length=50, null=False)
+
+class Format(models.Model):
+  format = models.CharField(max_length=50, null=False)
+
+class Formular(models.Model):
+  file = models.FileField(null=False)
+  dateFormRequest = models.DateTimeField(auto_now_add=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  office = models.ForeignKey(Office, on_delete=models.CASCADE)
+  format = models.ForeignKey(Format, on_delete=models.CASCADE)
+  building = models.ForeignKey(Building, on_delete=models.CASCADE)
