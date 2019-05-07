1af28ef581df245c29b13541ec22133a0a0c00f2
Ac <stephane.achari@u-psud.fr>
2019-05-07 16:50:17 +0200

    admin page

--- printingForm/admin.py
+++ printingForm/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Building
+from .models import Office
+from .models import Format
 
-# Register your models here.
+class BuildingAdmin(admin.ModelAdmin):
+  list_display = ('building',)
+
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Office)
+admin.site.register(Format)
